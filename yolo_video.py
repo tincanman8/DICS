3ae7342a7a146d079c3c7a0b8ce9ff73b83d365c
tincanman8 <amarmasalmeh@hotmail.com>
2018-11-30 23:39:52 +0000

    Update yolo_video.py

--- yolo_video.py
+++ yolo_video.py
@@ -24,7 +24,7 @@ ap.add_argument("-t", "--threshold", type=float, default=0.3,
 args = vars(ap.parse_args())
 
 # load the COCO class labels our YOLO model was trained on
-labelsPath = os.path.sep.join([args["yolo"], "objects.names"])
+labelsPath = os.path.sep.join([args["yolo"], "DICS.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
 # initialize a list of colors to represent each possible class label
@@ -34,8 +34,8 @@ LABELS = open(labelsPath).read().strip().split("\n")
 COLORS = np.array([[0, 0, 0],[0, 0, 255],[0, 255, 0],[0, 255, 255]])
 
 # derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join([args["yolo"], "yolov3-tiny.backup"])
-configPath = os.path.sep.join([args["yolo"], "yolov3-tiny.cfg"])
+weightsPath = os.path.sep.join([args["yolo"], "DICS.backup"])
+configPath = os.path.sep.join([args["yolo"], "DICS.cfg"])
 
 # load our YOLO object detector trained on COCO dataset (80 classes)
 # and determine only the *output* layer names that we need from YOLO
